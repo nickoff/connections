e1fde48a32986059141494b38f7c974311aa0f5f
Nickoff <v3136820@gmail.com>
2023-12-14 00:09:15 +0300

    feat: profile page implement update name validation

--- src/app/auth/pages/profile/profile.component.ts
+++ src/app/auth/pages/profile/profile.component.ts
@@ -1,14 +1,17 @@
 import { CommonModule, DatePipe } from '@angular/common';
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, OnInit } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { LoadingService } from 'src/app/core/services/loading/loading.services';
 import { NewNameModel, UserModel } from 'src/app/shared/models/user.model';
 import { selectUserValue, USER_ACTIONS } from 'src/app/store/user';
 
+import { ERROR_NAME_MESSAGE } from '../../constants';
+import { AuthNameErrors } from '../../enums';
 import { ProfileUserModel } from '../../models/profile-user.model';
+import { validateName } from '../../services/validate-name.service';
 
 @Component({
   selector: 'app-profile',
@@ -20,15 +23,22 @@ import { ProfileUserModel } from '../../models/profile-user.model';
 })
 export class ProfileComponent implements OnInit {
   isNameEditable = false;
+  isSubmitted = false;
+  isButtonDisabled = false;
   user$?: Observable<UserModel>;
   isLoading$ = this.loadingService.getLoading;
 
   profileUser: FormGroup<ProfileUserModel> = new FormGroup({
     email: new FormControl('', { nonNullable: true }),
     uid: new FormControl('', { nonNullable: true }),
-    name: new FormControl('', { nonNullable: true }),
+    name: new FormControl('', { nonNullable: true, validators: [Validators.required, validateName()] }),
     createdAt: new FormControl('', { nonNullable: true })
   });
+
+  name = this.profileUser.controls.name;
+
+  isShowNameError = false;
+  nameErrors?: string;
 
   constructor(
     private datePipe: DatePipe,
@@ -41,11 +51,11 @@ export class ProfileComponent implements OnInit {
   }
   ngOnInit(): void {
     this.setUserFromStore();
-  }
-
-  getLocalTime(timestamp: number): string {
-    const date = new Date(timestamp);
-    return this.datePipe.transform(date, 'medium') || '';
+    this.profileUser.valueChanges.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(() => {
+      this.refreshErrorsState();
+      this.profileUser.valid ? (this.isButtonDisabled = false) : (this.isButtonDisabled = true);
+      this.cdr.markForCheck();
+    });
   }
 
   editName(): void {
@@ -53,7 +63,12 @@ export class ProfileComponent implements OnInit {
   }
 
   editSave(): void {
+    this.isSubmitted = true;
+    this.refreshErrorsState();
+
+    if (!this.profileUser.valid) return;
     const updateName = this.profileUser.value.name;
+
     if (!updateName) return;
 
     const newName: NewNameModel = {
@@ -80,5 +95,28 @@ export class ProfileComponent implements OnInit {
       this.isNameEditable = false;
       this.cdr.markForCheck();
     });
+  }
+
+  private getShowNameError(): boolean {
+    return this.name.errors !== null && this.name.errors && (this.name.dirty || this.isSubmitted);
+  }
+
+  private getNameErrors(): string {
+    return (
+      this.name.errors !== null &&
+      ((this.name.errors[AuthNameErrors.onlyLettersOrSpaces] && ERROR_NAME_MESSAGE.onlyLettersOrSpaces) ||
+        (this.name.errors[AuthNameErrors.required] && ERROR_NAME_MESSAGE.required) ||
+        (this.name.errors[AuthNameErrors.maxLengthName] && ERROR_NAME_MESSAGE.maxLengthName))
+    );
+  }
+
+  private getLocalTime(timestamp: number): string {
+    const date = new Date(timestamp);
+    return this.datePipe.transform(date, 'medium') || '';
+  }
+
+  private refreshErrorsState(): void {
+    this.isShowNameError = this.getShowNameError();
+    this.nameErrors = this.getNameErrors();
   }
 }
