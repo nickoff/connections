c40191ac48aa0a8e592e83f94b1279d4277d4fa5
Nickoff <v3136820@gmail.com>
2023-12-16 23:20:16 +0300

    feat: add disabled button in pop-up view

--- src/app/connections/components/index.ts
+++ src/app/connections/components/index.ts
@@ -1,2 +1,3 @@
 export { GroupsListComponent } from './groups-list/groups-list.component';
+export { ModalDeleteGroupComponent } from './modal-delete-group/modal-delete-group.component';
 export { PeopleListComponent } from './people-list/people-list.component';
--- src/app/connections/components/modal-delete-group/modal-delete-group.component.ts
+++ src/app/connections/components/modal-delete-group/modal-delete-group.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectorRef, Component, DestroyRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatButtonModule } from '@angular/material/button';
 import {
@@ -8,22 +9,35 @@ import {
   MatDialogRef,
   MatDialogTitle
 } from '@angular/material/dialog';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { Store } from '@ngrx/store';
+import { LoadingService } from 'src/app/core/services/loading/loading.services';
 import { GROUPS_ACTIONS, selectGroupByID } from 'src/app/store/groups';
 
 @Component({
   selector: 'app-modal-delete-group',
   standalone: true,
-  imports: [MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent],
+  imports: [
+    MatButtonModule,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogTitle,
+    MatDialogContent,
+    CommonModule,
+    MatProgressBarModule
+  ],
   templateUrl: './modal-delete-group.component.html',
-  styleUrl: './modal-delete-group.component.scss'
+  styleUrl: './modal-delete-group.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ModalDeleteGroupComponent {
+  isLoading$ = this.loadingService.getLoading;
   constructor(
     public dialogRef: MatDialogRef<ModalDeleteGroupComponent>,
     private store: Store,
     private destroyRef: DestroyRef,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
+    private loadingService: LoadingService
   ) {}
 
   close(): void {
